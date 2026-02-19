a714155190a05c0f5805347fa3203a35f28ef1d0
apa97 <114135328+apa97@users.noreply.github.com>
2026-02-18 17:24:53 -0800

    a3 changes

--- vis.js
+++ vis.js
@@ -25,3 +25,77 @@ for (let i = 0; i < 20; i++) {
   circle.setAttribute("fill",`rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`);
   art.appendChild(circle);
 }
+
+// -------------------------------------------------------------------------
+
+
+// Visualization 1 - Global Sales by Genre & Platform
+vegaEmbed("#vis1", {
+  data: { url: "data/videogames_wide.csv" },
+  mark: "bar",
+  encoding: {
+    x: { field: "Platform", type: "nominal" },
+    y: { aggregate: "sum", field: "Global_Sales", type: "quantitative" },
+    color: { field: "Genre", type: "nominal" },
+    tooltip: [
+      { field: "Platform", type: "nominal" },
+      { field: "Genre", type: "nominal" },
+      { field: "Global_Sales", type: "quantitative" }
+    ]
+  },
+  width: "container",
+  height: 400
+});
+
+// Visualization 2 - Sales Over Time by Platform
+vegaEmbed("#vis2", {
+  data: { url: "data/videogames_wide.csv" },
+  mark: "line",
+  encoding: {
+    x: { field: "Year", type: "ordinal" },
+    y: { aggregate: "sum", field: "Global_Sales", type: "quantitative" },
+    color: { field: "Platform", type: "nominal" },
+    tooltip: [
+      { field: "Year", type: "ordinal" },
+      { field: "Platform", type: "nominal" },
+      { field: "Global_Sales", type: "quantitative" }
+    ]
+  },
+  width: "container",
+  height: 400
+});
+
+// Visualization 3 - Regional Sales by Platform
+vegaEmbed("#vis3", {
+  data: { url: "data/videogames_long.csv" },
+  mark: "bar",
+  encoding: {
+    x: { field: "Platform", type: "nominal" },
+    y: { aggregate: "sum", field: "Sales", type: "quantitative" },
+    color: { field: "Region", type: "nominal" },
+    tooltip: [
+      { field: "Platform", type: "nominal" },
+      { field: "Region", type: "nominal" },
+      { field: "Sales", type: "quantitative" }
+    ]
+  },
+  width: "container",
+  height: 400
+});
+
+// Visualization 4 - Visual Story: Top Genres Globally
+vegaEmbed("#vis4", {
+  data: { url: "data/videogames_wide.csv" },
+  mark: "bar",
+  encoding: {
+    y: { field: "Genre", type: "nominal" },
+    x: { aggregate: "sum", field: "Global_Sales", type: "quantitative" },
+    color: { field: "Genre", type: "nominal" },
+    tooltip: [
+      { field: "Genre", type: "nominal" },
+      { field: "Global_Sales", type: "quantitative" }
+    ]
+  },
+  width: "container",
+  height: 400
+});
